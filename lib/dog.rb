f00478721c0499932d788348e32d5d277767d864
Lessllie Real <rlessllie@yahoo.com>
2019-10-12 06:48:45 +0000

    Automatically backed up by Learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,17 @@
-class Dog 
-  def name=(dog_name)
-    this_dogs_name = dog_name
+class Dog
+ 
+  def name=(dogs_name)
+    @this_dogs_name = dogs_name
   end
  
   def name
-    this_dogs_name
+    @this_dogs_name
   end
-  lassie = Dog.new
+end
+ 
+lassie = Dog.new
 lassie.name = "Lassie"
  
-lassie.name #=> "Lassie"
-  
+puts lassie.name
 end 
 
